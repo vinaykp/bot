cca463a53b28e863c94ef27222680c6a9fec225e
vinaykp <vinaykp@hotmail.com>
2025-05-08 21:49:31 -0500

    fix instructions

--- control_agent/agent.py
+++ control_agent/agent.py
@@ -10,13 +10,12 @@ root_agent = Agent(
     name="control_agent",
     model="gemini-2.0-flash-exp",
     description=(
-        "Agent to answer questions about the time and weather in a city."
+        "Agent to control other agents."
     ),
     instruction=(
-        "You are a control agent that can call either the search agent or the multi tool agent. "
         "You will decide which agent to call based on the user's question."
         "If the question is about the time or weather in a city, call the multi tool agent. "
-        "If the question is about a student, call the student agent. "
+        "If the question is about a general search, call the search agent. "
         "If the question is about an inventory item, call the inventory agent."
     ),
     tools=[agent_tool.AgentTool(agent=search_agent), 
